8a9b5a88e3d8b247b79b93b17f4414ae7a860d27
IvanKotykUkr <iwankot3@gmail.com>
2022-06-07 19:39:39 +0100

    appdate

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import exspress, {Request,Response} from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+
 const app = exspress()
+app.use(cors())
+app.use(bodyParser.json())
+
 const port = process.env.PORT || 5000
+
 
 
 
@@ -14,14 +19,24 @@ const videos = [
     {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
     {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
 ]
-app.use(cors())
-app.use(bodyParser.json())
+
 
 app.get('/videos',(req:Request,res: Response)=>{
 
     res.send(videos)
 
 })
+
+app.get('/videos/:videoId',(req:Request,res:Response)=>{
+    const id= +req.params.videoId
+    const video = videos.find(v=>v.id===id)
+    if(!video){
+        res.sendStatus(404)
+    }else {
+        res.json(video)
+    }
+
+    })
 
 
 
@@ -53,7 +68,7 @@ app.put('/videos/:id',(req: Request, res: Response)=>{
         res.sendStatus(404)
     } else {
         video.title = req.body.title
-        res.json(video)
+        res.sendStatus(204)
     }
 })
 
